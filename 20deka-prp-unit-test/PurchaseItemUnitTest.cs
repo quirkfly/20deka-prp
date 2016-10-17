175783da1baf9ee81c72e4a9f1aef8f7fcc2082a
Peter Dermek <p3t3r.d3rm3k@gmail.com>
2016-10-17 18:36:37 +0200

    Completed unit tests.

--- 20deka-prp-unit-test/PurchaseItemUnitTest.cs
+++ 20deka-prp-unit-test/PurchaseItemUnitTest.cs
@@ -10,9 +10,31 @@ namespace TwentyDekaUnitTest
     {
         [TestMethod]
         [ExpectedException(typeof(InvalidPurchaseItemIdException))]
-        public void TestMissingItemId()
+        public void TestNullItemId()
         {
             PurchaseItem purchaseItem = new PurchaseItem(null, 3);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPurchaseItemIdException))]
+        public void TestEmptyItemId()
+        {
+            PurchaseItem purchaseItem = new PurchaseItem("", 3);
+        }
+
+        [TestMethod]
+        public void TestValidItemId()
+        {
+            PurchaseItem purchaseItem = new PurchaseItem("933920932", 3);
+            Assert.AreEqual("00000000000000000000000933920932", purchaseItem.itemId);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPurchaseItemIdException))]
+        public void TestItemIdLengthExceeded()
+        {
+            PurchaseItem purchaseItem = new PurchaseItem("34343343434343434343434343434343903434344348080804343808043408800843438080843", 3);
+        }
+
     }
 }
